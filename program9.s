22aa217358ef324ae40f9ea2a6fb7da84e151903
Catalina Macovei <cmacovei01@gmail.com>
2022-10-13 16:19:44 +0300

    Using LEA to create one-dimensional array

--- program9.s
+++ program9.s
@@ -2,27 +2,19 @@
     n: .long 5
     s: .space 4
 .text
-.globl main
+
+.globl main 
+
 main:
-    mov $0, %ecx
-    ;// initializam registrul contor cu 0
-
+    sub $1, n
+    mov n, %ecx
 etloop:
-    cmp  n, %ecx
-    je etexit
-    ;//daca %ecx == n am terminat parcurgerea
-
     add %ecx, s
-    ;// s += %ecx
+    loop etloop
+    jmp etexit
 
-    add $1, %ecx
-    ;// %ecx += 1
-
-    jmp etloop
-    ;// reiau parcurgerea
-
-    etexit:
-        mov $1, %eax
-        mov $0, %ebx
-        int 0x80
-        
+etexit:
+    mov $1, %eax
+    mov $0, %ebx
+    int $0x80
+    
