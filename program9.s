53569560547acc9854d115b8199362f67d9a8861
Catalina Macovei <cmacovei01@gmail.com>
2022-10-13 15:34:21 +0300

    Looping using counter register %ecx

---
+++ program9.s
@@ -0,0 +1,28 @@
+.data
+    n: .long 5
+    s: .space 4
+.text
+.globl main
+main:
+    mov $0, %ecx
+    ;// initializam registrul contor cu 0
+
+etloop:
+    cmp  n, %ecx
+    je etexit
+    ;//daca %ecx == n am terminat parcurgerea
+
+    add %ecx, s
+    ;// s += %ecx
+
+    add $1, %ecx
+    ;// %ecx += 1
+
+    jmp etloop
+    ;// reiau parcurgerea
+
+    etexit:
+        mov $1, %eax
+        mov $0, %ebx
+        int 0x80
+        
